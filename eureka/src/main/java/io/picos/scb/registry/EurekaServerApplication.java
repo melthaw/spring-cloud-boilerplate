84609c8ad084d84d4d6670aa34e32e262f45fd95
chenxue <melthaw@gmail.com>
2018-01-12 16:05:48 +0800

    add sub-proj eureka

--- eureka/src/main/java/io/picos/scb/registry/EurekaServerApplication.java
+++ eureka/src/main/java/io/picos/scb/registry/EurekaServerApplication.java
@@ -1,14 +1,16 @@
 package io.picos.scb.registry;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author dz
  */
-@EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
