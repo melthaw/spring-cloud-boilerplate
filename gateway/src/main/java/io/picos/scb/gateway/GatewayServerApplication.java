67830d68cc4d298774669ecfb14014826194c2e4
chenxue <melthaw@gmail.com>
2018-01-12 17:54:33 +0800

    fixed: build file

---
+++ gateway/src/main/java/io/picos/scb/gateway/GatewayServerApplication.java
@@ -0,0 +1,16 @@
+package io.picos.scb.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dz
+ */
+@SpringBootApplication
+public class GatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
+
+}
