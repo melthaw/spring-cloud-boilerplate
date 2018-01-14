52a72b2d57a08cbc4f083082f3faa36667bfc0a5
chenxue <melthaw@gmail.com>
2018-01-14 14:48:47 +0800

    add gateway

--- gateway/src/main/java/io/picos/scb/gateway/GatewayApplication.java
+++ gateway/src/main/java/io/picos/scb/gateway/GatewayApplication.java
@@ -2,6 +2,8 @@ package io.picos.scb.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
@@ -9,6 +11,8 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
+@EnableCaching
 public class GatewayApplication {
 
     public static void main(String[] args) {
---
+++ security/src/main/java/io/picos/scb/security/SecurityModuleConfiguration.java
@@ -0,0 +1,19 @@
+package io.picos.scb.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by dz on 2018/1/12.
+ */
+@Configuration
+public class SecurityModuleConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable();
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+
+}
