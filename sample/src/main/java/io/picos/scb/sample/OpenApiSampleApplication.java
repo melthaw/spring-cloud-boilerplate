95013c43b4dc870014d09389bce78414e41b743f
chenxue <melthaw@gmail.com>
2018-01-14 16:56:30 +0800

    make sample working

--- sample/src/main/java/io/picos/scb/sample/OpenApiSampleApplication.java
+++ sample/src/main/java/io/picos/scb/sample/OpenApiSampleApplication.java
@@ -1,31 +1,20 @@
 package io.picos.scb.sample;
 
-import io.picos.scb.sample.controller.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = SecurityAutoConfiguration.class)
 @EnableEurekaClient
 @EnableAsync
-public class OpenApiApplication  {
-
-    @Bean
-    public EurekaRestController eurekaRestController() {
-        return new EurekaRestController();
-    }
+public class OpenApiSampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(new Object[]{OpenApiApplication.class}, args);
+        SpringApplication.run(new Object[]{OpenApiSampleApplication.class}, args);
     }
 
 }
--- sample/src/main/java/io/picos/scb/sample/controller/SampleRestController.java
+++ sample/src/main/java/io/picos/scb/sample/controller/SampleRestController.java
@@ -5,24 +5,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class EurekaRestController {
+public class SampleRestController {
 
     @RequestMapping("/")
     @ResponseBody
     public String home() {
-        return "hello cloud";
-    }
-
-    @RequestMapping("/info")
-    @ResponseBody
-    public String info() {
-        return "sample api";
-    }
-
-    @RequestMapping("/health")
-    @ResponseBody
-    public String health() {
-        return "ok";
+        return "Welcome to spring cloud sample.";
     }
 
 }
